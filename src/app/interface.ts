707588487510ccd41be39d2d020ab5b3a06ceac4
= <=>
2023-05-21 00:40:34 -0600

    interface from response

---
+++ src/app/interface.ts
@@ -0,0 +1,25 @@
+interface ApiGen {
+    id: string;
+    object: string;
+    created: number;
+    model: string;
+    usage: Usage;
+    choices: Choice[];
+}
+
+interface Choice {
+  message: Message;
+  finish_reason: string;
+  index: number;
+}
+
+export interface Message {
+  role: string;
+  message: string;
+}
+
+interface Usage {
+  prompt_tokens: number;
+  completion_tokens: number;
+  total_tokens: number;
+}
